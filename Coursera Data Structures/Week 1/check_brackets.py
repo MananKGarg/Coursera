bd5199ed6da336c65f789145ce227f6fd8726134
Manan Kumar Garg <62146744+MananKGarg@users.noreply.github.com>
2021-01-25 00:16:29 +0530

    Create check_brackets.py

---
+++ Coursera Data Structures/Week 1/check_brackets.py
@@ -0,0 +1,50 @@
+# python3
+
+from collections import namedtuple
+
+Bracket = namedtuple("Bracket", ["char", "position"])
+
+
+def are_matching(left, right):
+    return (left + right) in ["()", "[]", "{}"]
+
+
+def find_mismatch(text):
+    opening_brackets_stack = []
+    opening_bracket_position_stack = []
+    for i, next in enumerate(text):
+        if next in "([{":
+            opening_brackets_stack.append(next)
+            opening_bracket_position_stack.append(i+1)
+            pass
+
+        if next in ")]}":
+            if len(opening_brackets_stack) == 0:
+                return i + 1
+            elif not are_matching(opening_brackets_stack[-1], next):
+                return i + 1
+            elif are_matching(opening_brackets_stack[-1], next):
+                opening_brackets_stack.pop(-1)
+                opening_bracket_position_stack.pop(-1)
+
+            pass
+
+    if len(opening_brackets_stack)!= 0:
+        return opening_bracket_position_stack[0]
+
+    return False
+
+
+
+def main():
+    text = input()
+    mismatch = find_mismatch(text)
+    if mismatch == False:
+        print("Success")
+    else:
+        print(mismatch)
+
+
+
+if __name__ == "__main__":
+    main()
